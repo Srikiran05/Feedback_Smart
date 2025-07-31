09e581114fd35fa72567f1740e7f71d3670fb320
Srikiran05 <88445876+Srikiran05@users.noreply.github.com>
2025-07-31 10:55:13 +0530

    Update constants.js

--- feedback/src/constants.js
+++ feedback/src/constants.js
@@ -1,4 +1,4 @@
 export const SENTIMENT_STRING = ['Worst', 'Average', 'Excellent'];
 
 export const CATEGORY = ['ambience', 'cleanliness', 'taste', 'service', 'value'];
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://smart-cafe-feedback.onrender.com';

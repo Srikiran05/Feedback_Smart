99f6d81bf7fd81d1cc148cbf9a6a7ec0ffc6f757
Srikiran05 <malladisrikiran@gmail.com>
2025-07-31 12:21:12 +0530

    NA

--- feedback/src/component/Dashboard.jsx
+++ feedback/src/component/Dashboard.jsx
@@ -45,6 +45,8 @@ const Dashboard = () => {
 
             const totalFeedbacks = data.total_feedbacks || 0;
             console.log('Total Feedbacks:', totalFeedbacks);
+            console.log('Average rating:', average_rating);
+            console.log('Total responses:', responses_today);
 
             setStats({
                 total_feedbacks: totalFeedbacks,
